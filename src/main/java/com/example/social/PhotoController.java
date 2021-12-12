c282f1fd772d6b168c9b36bab0ecd97d52a149e0
Roy Chan <roy@selfdecode.com>
2021-12-12 23:47:44 +0800

    change to /api/photo

--- src/main/java/com/example/social/PhotoController.java
+++ src/main/java/com/example/social/PhotoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ import com.example.social.security.JwtAuthentication;
 import com.example.social.utils.S3Utils;
 
 @RestController
+@RequestMapping(path = "api")
 public class PhotoController {
 	public static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
 	public static final long EXPIRED_DAYS = 7;
